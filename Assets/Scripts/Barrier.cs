5816a00c93da5ccef20881eccf0ac3091040ad2f
cri <a110082040@mail.shu.edu.tw>
2022-11-09 01:57:44 +0800

    Revert "Added pause button, pause menu, resume, restart, and quit"
    This reverts commit a5a6e86f4b6ded1279c197780af8c1cbf66d2c71.

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -6,7 +6,7 @@ public class Barrier : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Bullet") 
+        if (other.tag == "bullet") 
         {
             Destroy(other.gameObject);
         }
